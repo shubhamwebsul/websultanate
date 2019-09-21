14320365f49980535d16da8987b648a53b54f389
Alok <alok@websultanate.com>
2019-09-21 18:17:46 +0530

    Updates

---
+++ precache-manifest.76adce266ac9362517a45cafb613dff0.js
@@ -0,0 +1,270 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5bb1c64a4feaffad346187a9adb123f",
+    "url": "/websultanate/index.html"
+  },
+  {
+    "revision": "3b8089025e25cf52516d",
+    "url": "/websultanate/static/css/2.cc82b09a.chunk.css"
+  },
+  {
+    "revision": "aa1e7dafebb7e0c2a0ea",
+    "url": "/websultanate/static/css/main.1408c02b.chunk.css"
+  },
+  {
+    "revision": "3b8089025e25cf52516d",
+    "url": "/websultanate/static/js/2.b17127f4.chunk.js"
+  },
+  {
+    "revision": "aa1e7dafebb7e0c2a0ea",
+    "url": "/websultanate/static/js/main.d79a61a6.chunk.js"
+  },
+  {
+    "revision": "46a0672394888a9e3360",
+    "url": "/websultanate/static/js/runtime~main.9ddb9a6b.js"
+  },
+  {
+    "revision": "db16b779669fc10b41e76bc928b6c668",
+    "url": "/websultanate/static/media/10C.db16b779.svg"
+  },
+  {
+    "revision": "573f415adb44ab4ac9e214dd589eabf4",
+    "url": "/websultanate/static/media/10D.573f415a.svg"
+  },
+  {
+    "revision": "5513cf62554bd17268a4c0d6f4680dbf",
+    "url": "/websultanate/static/media/10H.5513cf62.svg"
+  },
+  {
+    "revision": "23ede056b48e7592bc19ef05148fa789",
+    "url": "/websultanate/static/media/10S.23ede056.svg"
+  },
+  {
+    "revision": "b77aeebb064f362a418212f81ab32d08",
+    "url": "/websultanate/static/media/2C.b77aeebb.svg"
+  },
+  {
+    "revision": "4fce45b07de69d86bc20223f2591b65e",
+    "url": "/websultanate/static/media/2D.4fce45b0.svg"
+  },
+  {
+    "revision": "f638d3c2da3d818c44a094e7c40abed0",
+    "url": "/websultanate/static/media/2H.f638d3c2.svg"
+  },
+  {
+    "revision": "4b5257d257fcf41bc28d11b0339c3818",
+    "url": "/websultanate/static/media/2S.4b5257d2.svg"
+  },
+  {
+    "revision": "98b61fae6cddbf9298959d5651acfdf9",
+    "url": "/websultanate/static/media/3C.98b61fae.svg"
+  },
+  {
+    "revision": "96528f64b1b8e15fbd9a12a6f34bc973",
+    "url": "/websultanate/static/media/3D.96528f64.svg"
+  },
+  {
+    "revision": "ad7e368f2940657044e175408300d634",
+    "url": "/websultanate/static/media/3H.ad7e368f.svg"
+  },
+  {
+    "revision": "c7f35b10d84a67bdd748abf734383f25",
+    "url": "/websultanate/static/media/3S.c7f35b10.svg"
+  },
+  {
+    "revision": "d975ef336c1d7bdb34c5592214f6e7f8",
+    "url": "/websultanate/static/media/4C.d975ef33.svg"
+  },
+  {
+    "revision": "f7f31a119c70aa4a6698da51fb2b9724",
+    "url": "/websultanate/static/media/4D.f7f31a11.svg"
+  },
+  {
+    "revision": "8a079278c4b5e18425ace50ae24815ef",
+    "url": "/websultanate/static/media/4H.8a079278.svg"
+  },
+  {
+    "revision": "5a3abe179e8e49c61b36382b306ebbab",
+    "url": "/websultanate/static/media/4S.5a3abe17.svg"
+  },
+  {
+    "revision": "e5373949d97278d0e3a89355b7886495",
+    "url": "/websultanate/static/media/5C.e5373949.svg"
+  },
+  {
+    "revision": "dd47c8c56ea20f025dddf4f5fecc4afb",
+    "url": "/websultanate/static/media/5D.dd47c8c5.svg"
+  },
+  {
+    "revision": "a08a2297764baa30cabc54650d9e1493",
+    "url": "/websultanate/static/media/5H.a08a2297.svg"
+  },
+  {
+    "revision": "6be904f7f4b9d5f4fa99c645ad9feafd",
+    "url": "/websultanate/static/media/5S.6be904f7.svg"
+  },
+  {
+    "revision": "16393376c7a8deda993384a6ef7f829d",
+    "url": "/websultanate/static/media/6C.16393376.svg"
+  },
+  {
+    "revision": "b0ede146e4c0d9fec04f20132958ed0f",
+    "url": "/websultanate/static/media/6D.b0ede146.svg"
+  },
+  {
+    "revision": "7a6be3999416d11818a3b8d5e21526bd",
+    "url": "/websultanate/static/media/6H.7a6be399.svg"
+  },
+  {
+    "revision": "497b52bef49d68bd21e130c2ccb12467",
+    "url": "/websultanate/static/media/6S.497b52be.svg"
+  },
+  {
+    "revision": "77a8441baa07895070336c5040e1fb69",
+    "url": "/websultanate/static/media/7C.77a8441b.svg"
+  },
+  {
+    "revision": "06981b338bdd6cfe381370544c092788",
+    "url": "/websultanate/static/media/7D.06981b33.svg"
+  },
+  {
+    "revision": "dfc55bdb9ae0aefbe291ec0cf1e39791",
+    "url": "/websultanate/static/media/7H.dfc55bdb.svg"
+  },
+  {
+    "revision": "c0b69b7a96da62f75035c1725f739e27",
+    "url": "/websultanate/static/media/7S.c0b69b7a.svg"
+  },
+  {
+    "revision": "a359ce5e306453eb1aa9932e07d11836",
+    "url": "/websultanate/static/media/8C.a359ce5e.svg"
+  },
+  {
+    "revision": "9a486f2858b934c2c9a151d1231e7c43",
+    "url": "/websultanate/static/media/8D.9a486f28.svg"
+  },
+  {
+    "revision": "172772a7d03c38ab08177d8c2984ff70",
+    "url": "/websultanate/static/media/8H.172772a7.svg"
+  },
+  {
+    "revision": "0cfff1c16fc8aa4376bc9294b9258078",
+    "url": "/websultanate/static/media/8S.0cfff1c1.svg"
+  },
+  {
+    "revision": "9a06579808ce5e8fb7a963e69dc9d537",
+    "url": "/websultanate/static/media/9C.9a065798.svg"
+  },
+  {
+    "revision": "db05fa82fc6076a1d9a3045740ada0cf",
+    "url": "/websultanate/static/media/9D.db05fa82.svg"
+  },
+  {
+    "revision": "7d46b44037d8db768ee3a14c140b4136",
+    "url": "/websultanate/static/media/9H.7d46b440.svg"
+  },
+  {
+    "revision": "a17ba13f0edbe189b48aad933d72aea8",
+    "url": "/websultanate/static/media/9S.a17ba13f.svg"
+  },
+  {
+    "revision": "9704190ad1c974e15a2dded5290190bc",
+    "url": "/websultanate/static/media/AC.9704190a.svg"
+  },
+  {
+    "revision": "1f544e310c75d212d76a494bd091f64e",
+    "url": "/websultanate/static/media/AD.1f544e31.svg"
+  },
+  {
+    "revision": "375c6c20c5b6c8b2caa9e145e5d2e122",
+    "url": "/websultanate/static/media/AH.375c6c20.svg"
+  },
+  {
+    "revision": "a3de388304b8eff0fca61785fd48585b",
+    "url": "/websultanate/static/media/AS.a3de3883.svg"
+  },
+  {
+    "revision": "d4b571c29c447c0588213ae428b142fc",
+    "url": "/websultanate/static/media/JC.d4b571c2.svg"
+  },
+  {
+    "revision": "a557db8ea1081c338f00447e76bb767c",
+    "url": "/websultanate/static/media/JD.a557db8e.svg"
+  },
+  {
+    "revision": "f577e0e2a94417f104f05bc90299c08e",
+    "url": "/websultanate/static/media/JH.f577e0e2.svg"
+  },
+  {
+    "revision": "0c597a9b6a25b72a6ee667f17eff412d",
+    "url": "/websultanate/static/media/JS.0c597a9b.svg"
+  },
+  {
+    "revision": "2cee6ad0dd3bd9ed01f0a6a420daf10e",
+    "url": "/websultanate/static/media/KC.2cee6ad0.svg"
+  },
+  {
+    "revision": "5ef07a0f3de32e413db33e1717c3fd8c",
+    "url": "/websultanate/static/media/KD.5ef07a0f.svg"
+  },
+  {
+    "revision": "d995a3daa9c28f662ffc0a220657a105",
+    "url": "/websultanate/static/media/KH.d995a3da.svg"
+  },
+  {
+    "revision": "a58a77c8b70db2fc0aaf889a52db44e0",
+    "url": "/websultanate/static/media/KS.a58a77c8.svg"
+  },
+  {
+    "revision": "5007127f8d841f1b2e3fd4162099baab",
+    "url": "/websultanate/static/media/QC.5007127f.svg"
+  },
+  {
+    "revision": "05bef93af7de2d69aa4b1ddabbac6f53",
+    "url": "/websultanate/static/media/QD.05bef93a.svg"
+  },
+  {
+    "revision": "a667004464e546b7c0c0e684d00d93ac",
+    "url": "/websultanate/static/media/QH.a6670044.svg"
+  },
+  {
+    "revision": "f32ea87760fc8cd4f40954e8fd6b4b5d",
+    "url": "/websultanate/static/media/QS.f32ea877.svg"
+  },
+  {
+    "revision": "2b0f4c1dfad657cc38bfb7a1f26307c1",
+    "url": "/websultanate/static/media/baccarat_bg.2b0f4c1d.svg"
+  },
+  {
+    "revision": "183b6b59c8ce93ffe97db177db85d1bf",
+    "url": "/websultanate/static/media/baccarat_bg_small.183b6b59.svg"
+  },
+  {
+    "revision": "f0641e69176833da9f9dc83331d11f56",
+    "url": "/websultanate/static/media/baccarat_game_bg.f0641e69.svg"
+  },
+  {
+    "revision": "6fcd5d6517fb346ea61c6495c8746716",
+    "url": "/websultanate/static/media/cardimage.6fcd5d65.svg"
+  },
+  {
+    "revision": "b4b4ffd8b9f2730f38ef84f3ba874dc5",
+    "url": "/websultanate/static/media/chip_dot.b4b4ffd8.svg"
+  },
+  {
+    "revision": "d18aeb37dfcfa4974711c1727a4ccd3e",
+    "url": "/websultanate/static/media/plinko_bg.d18aeb37.svg"
+  },
+  {
+    "revision": "69bae178381b64ee3fa52488108991e0",
+    "url": "/websultanate/static/media/plinko_bg_small.69bae178.svg"
+  },
+  {
+    "revision": "38492bf05b67cd5569bbbeaf57f3121f",
+    "url": "/websultanate/static/media/rotate.38492bf0.svg"
+  },
+  {
+    "revision": "97f713e7c12259971af9eeb5f176cac6",
+    "url": "/websultanate/static/media/undo.97f713e7.svg"
+  }
+]);
